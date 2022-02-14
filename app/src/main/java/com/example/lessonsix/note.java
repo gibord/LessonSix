1c1bd34d1d32a1a67100ce22fc45055b90939301
gibord <gibord003@gmail.com>
2022-02-14 09:31:23 +0300

    Создал фрагменты приступил к ДЗ №6

---
+++ app/src/main/java/com/example/lessonsix/note.java
@@ -0,0 +1,30 @@
+package com.example.lessonsix;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class note extends Fragment {
+
+    public static note newInstance(String param1, String param2) {
+        note fragment = new note();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/lessonsix/noteContent.java
@@ -0,0 +1,32 @@
+package com.example.lessonsix;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class noteContent extends Fragment {
+
+
+    public static noteContent newInstance() {
+        noteContent fragment = new noteContent();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_content, container, false);
+    }
+}
